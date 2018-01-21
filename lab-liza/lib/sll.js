4d5d0eb1ff9e1d1f98b4fb612212a6956fd2635f
Victoria Oh <veoh1989@gmail.com>
2018-01-20 19:05:29 -0800

    sll.js remove method is complete

---
+++ lab-liza/lib/sll.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Nd = require('./nd');
+
+class SLL {
+  constructor() {
+    this.head = null;
+  }
+
+  insertHead(val) {
+    let nd = new Nd(val);
+
+    nd.next = this.head;
+    this.head = nd;
+    return this;
+  }
+
+  insertEnd(val) {
+    let nd = new Nd(val);
+
+    if(!this.head) {
+      this.head = nd;
+      return this;
+    }
+
+    for(var itr = this.head; itr.next; itr = itr.next);
+    itr.next = nd;
+    return this;
+  }
+
+  remove() {
+    let curr = this.head;
+    let prev = null;
+    let next = null;
+
+    while(curr) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    this.head = prev;
+    return this;
+  }
+}
+
+module.exports = SLL;
