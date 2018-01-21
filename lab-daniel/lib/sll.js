3febfd7b88f3b5f32d8ee6303d8f0710031b09ba
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-01-20 18:46:02 -0800

    finished building find Nth and find Nth from last functions and tested to verify functionality

--- lab-daniel/lib/sll.js
+++ lab-daniel/lib/sll.js
@@ -5,42 +5,42 @@ const Nd = require('./nd');
 class SLL {
   constructor (){
     this.head = null;
-    this.llen = 0;
+    this.lLen = 0;
   }
   insertHead (val) {
     let nd = new Nd(val);
     nd.next = this.head;
     this.head = nd;
-    this.llen++;
+    this.lLen++;
     return this;
   }
 
-  insertEnd (val) {
+  insertTail (val) {
     let nd = new Nd(val);
     
     if (!this.head) {
       this.head = nd;
-      this.llen++;
+      this.lLen++;
       return this;
     }
 
     for (var itr = this.head; itr.next; itr = itr.next);
     itr.next = nd;
-    this.llen++;
+    this.lLen++;
     return this;
   }
   remove (rem) {
     if (!rem) return null;
     if (rem === 1) { 
       this.head = this.head.next;
-      this.llen--;
+      this.lLen--;
       return this;
     }
     let curNode = this.head, preNode = null;
-    for (var loc = 0; loc < rem; loc++) {
-      if (loc === rem - 1) {
+    for (var loc = 1; loc <= rem; loc++) {
+      if (loc === rem) {
         preNode.next = curNode.next;
-        this.llen--;
+        this.lLen--;
         return this;
       }
       preNode = curNode;
@@ -60,8 +60,25 @@ class SLL {
     return this;
   }
 
-  findNthNode() {
+  findNthNode(n) {
+    if (!n || typeof n !== 'number') return null;
+    if (n > this.lLen || n < 1) return null;
+    let curNode = this.head;
+    for (let i = 1; i < n; i++) {
+      curNode = curNode.next;
+    }
+    return curNode;
+  }
 
+  findNthFromLastNode(n) {
+    if (n !== 0 && !n || typeof n !== 'number') return null;
+    if (n > this.lLen || n < 0) return null;
+    let curNode = this.head, nLoc = this.lLen - n;
+    for (let i = 1; i < nLoc; i++) {
+      curNode = curNode.next;
+    }
+    console.log(curNode);
+    return curNode;
   }
 }
 
