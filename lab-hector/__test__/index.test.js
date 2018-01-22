7796a18374c42aa116784fca7c1ecf068161231f
Hector <norzagaray@me.com>
2018-01-21 18:21:55 -0800

    scaffold for tests

--- lab-hector/__test__/index.test.js
+++ lab-hector/__test__/index.test.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const Nd = require('../lib/nd');
+require('jest');
+
+describe('reader module', function(){
+  describe('#reader', ()=>{
+    it('should...', () => {});
+    it('should...', () => {});
+    it('should...', () => {});
+  });
+  describe('#writer', () => {
+    it('should...', () => { });
+    it('should...', () => { });
+    it('should...', () => { });
+  });
+});
