ead6795dfe54d593abc9fb0628f0c587307d6a25
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-21 19:21:42 -0800

    add remove

--- lab-ed/lib/nd.js
+++ lab-ed/lib/nd.js
@@ -6,5 +6,4 @@ class Node {
     this.next = null
   }
 }
-
 module.exports = Node
--- lab-ed/lib/sll.js
+++ lab-ed/lib/sll.js
@@ -9,7 +9,6 @@ class SLL {
 
   insertHead(val) {
     let nd = new Nd(val)
-
     nd.next = this.head
     this.head = nd
     return this
@@ -17,24 +16,42 @@ class SLL {
 
   insertEnd(val) {
     let nd = new Nd(val)
-
     if(!this.head) {
       this.head = nd
       return this
     }
-
-    for(var itr = this.head; itr.next; itr = itr.next);
-    itr.next = nd
+    for(var itr = this.head; itr.next; itr = itr.next)
+      ;itr.next = nd
     return this
   }
 
   remove(offset) {
+    if (!offset) return null
+    if (offset === 0) return null
     if (this.head.next === null) return null
-  }
-
-  findNthNode() {
-
+    if(offset === 1) { 
+      this.head = this.head.next
+      return this
+    }
+    let thisOne = this.head
+    let lastOne
+    for(var itr = 1; itr <= offset; itr++) {
+      if (itr === offset) {
+        if (thisOne.next) {
+          lastOne.next = thisOne.next
+        } else {
+          lastOne.next = null
+        }
+        return this
+      }
+      lastOne = thisOne
+      thisOne = thisOne.next
+    }
   }
 }
+
+//   reverse() {
+  
+// }
 
 module.exports = SLL
