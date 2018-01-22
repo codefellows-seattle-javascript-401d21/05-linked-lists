8e2703b00b8f766c150905c6ab73f2402e564193
Hector <norzagaray@me.com>
2018-01-21 20:04:40 -0800

    added some tests

--- lab-hector/__test__/index.test.js
+++ lab-hector/__test__/index.test.js
@@ -7,8 +7,8 @@ require('jest');
 let test1 = new SLL(), test2 = new SLL();
 
 describe('constructor SLL', function () {
-  describe('#SLL Head', function () {
-    it('should...', function (){
+  describe('#SLL constructor', function () {
+    it('should test to see if there is a list...', function (){
       expect(test1).toBeInstanceOf(SLL);
       expect(test2).toBeInstanceOf(SLL);
     });
@@ -16,36 +16,84 @@ describe('constructor SLL', function () {
       expect(test1.head).toBeNull();
       expect(test2.head).toBeNull();
     });
-    it('should initialixe listLength as numeric 0', function() {
+    it('should initialize listLength as numeric 0', function() {
       expect(test1.listLength).toBe(0);
       expect(test2.listLength).toBe(0);
     });
   });
-  // describe('reader module', function () {
-  //   describe('#insert Head', () => {
-  //     it('should...', () => {});
-  //     it('should...', () => {});
-  //     it('should...', () => {});
-  //   });
-  //   describe('#insert tail', () => {
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //   });
-  //   describe('#remove', () => {
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //   });
-  //   describe('#reverse', () => {
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //   });
-  //   describe('#findNthNode', () => {
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //     it('should...', () => { });
-  //   });
-  // });
 });
+
+// This check if something was inserted in the HEAD
+describe('This checks the HEAD', function () {
+  describe('#SSL Head', function () {
+    it('should check if anything is in Head', function () {
+      expect(test1.head).toBeNull();
+      expect(test2.head).toBeNull();
+    });
+    it('should check if list is undefined', function () {
+      expect(test.listLength).toBe();
+      expect(test.listLength).toBe();
+    });
+    it('should...', function () {
+      expect().toBe();
+      expect().toBe();
+    });
+  });
+});
+
+//this is the test to check if inserted at TAIL
+
+// describe('This checks the TAIL', function () {
+//   describe('#SSL Tail', function () {
+//     it('should...', function () {
+//       expect().toBeInstanceOf();
+//       expect().toBeInstanceOf();
+//     });
+//     it('should...', function () {
+//       expect().toBeNull();
+//       expect().toBeNull();
+//     });
+//     it('should...', function () {
+//       expect().toBe();
+//       expect().toBe();
+//     });
+//   });
+// });
+
+
+// check if reversed
+// describe('This checks if it was reversed', function () {
+//   describe('#Reversed', function () {
+//     it('should...', function () {
+//       expect().toBeInstanceOf();
+//       expect().toBeInstanceOf();
+//     });
+//     it('should...', function () {
+//       expect().toBeNull();
+//       expect().toBeNull();
+//     });
+//     it('should...', function () {
+//       expect().toBe();
+//       expect().toBe();
+//     });
+//   });
+// });
+
+// this one checks to findNthNode
+
+// describe('This finds the NthNode', function () {
+//   describe('#SLL NthNode', function () {
+//     it('should...', function () {
+//       expect().toBe();
+//       expect().toBe();
+//     });
+//     it('should...', function () {
+//       expect().toBeNull();
+//       expect().toBeNull();
+//     });
+//     it('should...', function () {
+//       expect().toBe(0);
+//       expect().toBe(0);
+//     });
+//   });
+// });
