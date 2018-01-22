20ef599ad8ddee781f7e49cfd2e672df12bbd5b7
Jordan Van Ness <jordanwvn@gmail.com>
2018-01-21 18:32:50 -0800

    completed methods and prepared for testing

---
+++ lab-jordan/__test__/sll.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let SLL = require('../lib/sll.js');
+
+describe('SLL module', function () {
+  describe('#insert head', function () {
+    it('should ')
+    it('should ')
+    it('should ')
+  })
+  describe('#insert end', function () {
+    it('should ')
+    it('should ')
+    it('should ')
+  })
+  describe('#remove from offset', function () {
+    it('should ')
+    it('should ')
+    it('should ')
+  })
+  describe('#reverse', function () {
+    it('should ')
+    it('should ')
+    it('should ')
+  })
+  describe('#find nth node from end', function () {
+    it('should ')
+    it('should ')
+    it('should ')
+  })
+
+})
