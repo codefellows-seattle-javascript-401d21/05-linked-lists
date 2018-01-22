ad1e68a23654ed15485be0ca499ab3d2b9c67c68
Mitchell M <mitchellm1618@gmail.com>
2018-01-21 21:23:43 -0800

    one test working, refactoring code

--- lab-mitchell/__test__/sll.test.js
+++ lab-mitchell/__test__/sll.test.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const SLL = require('../lib/sll');
+
+describe('sll.js', () => {
+  it('new SLL constructor should create a new head node, with no value or next', (done) => {
+    let head = new SLL();
+    expect(head.head).toEqual(null);
+    expect(head.value).toEqual(undefined);
+    expect(head.next).toEqual(undefined);
+    done();
+  });
+
+  it('should ', (done) => {
+    let node = new SLL();
+    node.append(new SLL())
+
+    expect(node.head).toEqual(null);
+    done();
+  });
+
+  it('should ', (done) => {
+    let node = new SLL();
+    expect(node.head).toEqual(null);
+    done();
+  });
+
+  it('should ', (done) => {
+    let node = new SLL();
+    expect(node.head).toEqual(null);
+    done();
+  });
+});
--- lab-mitchell/lib/sll.js
+++ lab-mitchell/lib/sll.js
@@ -1,36 +1,65 @@
 'use strict';
 
-const Nd = require('./lib/nd');
+const Nd = require('../lib/nd');
 
 class SLL {
-  constructor() {
-    this.head = null;
+  constructor(val) {
+    this.head = this;
+    this.value = val;
+    this.next = null;
   }
 
   insertHead(val) {
-    let nd = new Nd(val);
-    nd.next = this.head;
-    this.head = nd;
+    let node = new SLL(val);
+    node.next = this.head;
+    this.head = node;
     return this;
   }
 
   insertEnd(val) {
-    let nd = new Nd(val);
+    let nd = new SLL(val);
     if(!this.head) {
       this.head = nd;
       return this;
     }
-    
     // for(var i = this.head; i.next; i = i.next);
     // i.next = nd;
     // return this;
   }
 
+  //O(n)
   reverse() {
+    if(!this.head || this.head.next) {
+      return 'Empty or single element linked list, nothing to reverse';
+    }
 
+    let result = null;
+    let current = this.head;
+    let temp;
+
+    while(current) {
+      temp = current.next;
+      current.next = result;
+      result = current;
+      current = temp;
+    }
+    return result;
   }
 
-  remove(offset) {
-
+  remove(val) {
+    if(!this.next) {
+      return this;
+    }
+    if(this === val) {
+      this.value = this.next.value;
+      this.next = this.next.next;
+    } else if (this.next === val) {
+      this.next = this.next.next;
+    } else {
+      this.next.remove(val);
+    }
+    return this;
   }
 }
+
+module.exports = SLL;
