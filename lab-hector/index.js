ade0dea775fe99afbe02aac66606aafd49ce7c4c
Hector <norzagaray@me.com>
2018-01-21 16:01:47 -0800

    sll and nd code

--- lab-hector/index.js
+++ lab-hector/index.js
@@ -1,1 +1,14 @@
 'use strict';
+//function Node(val) {
+  //this.val= val;
+//   this.next = null
+// }
+
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null
+  }
+}
+export default Node;
