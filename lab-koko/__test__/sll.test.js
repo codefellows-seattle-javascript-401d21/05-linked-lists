9f128eb90cb30eb4c34a854be0afda28367ad7f2
Koko Kassa <kowserk7@gmail.com>
2018-01-22 21:11:59 -0800

    fixed my function

--- lab-koko/__test__/sll.test.js
+++ lab-koko/__test__/sll.test.js
@@ -3,16 +3,48 @@
 const SLL = require('../lib/sll.js');
 
 describe ('Solution Module', () => {
-  describe ('SLL', () => {
-    it ('should check for an object', () => {
-      var list1 = new SLL();
-      
-      list1.insertEnd(2);
-      list1.insertEnd(4);
-      list1.insertEnd(6);
-      list1.insertEnd(8);
-      list1.insertEnd(10);
-      expect (list1.reverse()).toBeInstanceOf();
+  describe ('head', () => {
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+  });
+  describe ('end', () => {
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+  });
+  describe ('remove', () => {
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+  });
+  describe ('reverse', () => {
+    it ('', () => {
+
+    });
+    it ('', () => {
+
+    });
+    it ('', () => {
+
     });
   });
 });
