cf4e4f93f0b2fb57e2394c14989014474a4ea52e
Koko Kassa <kowserk7@gmail.com>
2018-01-28 17:03:35 -0800

    added test cases

---
+++ lab-koko/README.md
@@ -0,0 +1,25 @@
+### Linked Lists
+
+## Insert Head
+
+This function creates the head node of the linked list.
+Big O of O(n)
+
+## Insert End
+
+This fucntion adds the node to the head of the linked list.
+Big O of O(n)
+
+## Remove
+
+This function mevoes a specidfied node from the linked list.
+Big O of O(n)
+
+## Reverse 
+This takes the entire linked list and returns it in reverse order.
+Big O of O(n)
+
+##Installation
+
+To install this application, fork and git clone this repository into your local machine. Locate the directory lab-koko install the dependent packages.
+
--- lab-koko/__test__/sll.test.js
+++ lab-koko/__test__/sll.test.js
@@ -4,47 +4,50 @@ const SLL = require('../lib/sll.js');
 
 describe ('Solution Module', () => {
   describe ('head', () => {
-    it ('', () => {
-
+    let list1 = new SLL;
+    it ('should return an empty value if head is empty', () => {
+      expect(list1.insertHead()).toEqual({'head': {'next': null, 'value': undefined}});
     });
-    it ('', () => {
-
-    });
-    it ('', () => {
-
+    it ('should return a the value of head as 3', () => {
+      expect(list1.insertHead(3)).toEqual({'head': {'next': {'next': null, 'value': undefined}, 'value': 3}});
     });
   });
   describe ('end', () => {
-    it ('', () => {
-
+    let list2 = new SLL;
+    it ('should have a value of null when there is no arguemnt', () => {
+      expect(list2.insertEnd()).toEqual({'head': {'next': null, 'value': undefined}});
     });
     it ('', () => {
-
-    });
-    it ('', () => {
-
+      expect(list2.insertHead(7)).toEqual({'head': {'next': {'next': null, 'value': undefined}, 'value': 7}});
     });
   });
   describe ('remove', () => {
-    it ('', () => {
+    let list3 = new SLL;
 
-    });
-    it ('', () => {
+    list3.insertHead(2);
+    list3.insertEnd(5);
+    list3.insertEnd(7);
+    list3.insertEnd(12);
+    list3.insertEnd(20);
 
-    });
-    it ('', () => {
-
+    // it ('should remove the object at value 7', () => {
+    //   expect(list3.remove(7)).toEqual();
+    // });
+    it ('should require a value to remove', () => {
+      expect(list3.remove()).toBeNull();
     });
   });
   describe ('reverse', () => {
-    it ('', () => {
+    let list4 = new SLL;
 
-    });
-    it ('', () => {
+    list4.insertHead(2);
+    list4.insertEnd(5);
+    list4.insertEnd(7);
+    list4.insertEnd(12);
+    list4.insertEnd(20);
 
-    });
-    it ('', () => {
-
+    it ('should return the reverse order of the linked list', () => {
+      expect(list4.reverse()).toEqual({'head': {'next': {'next': {'next': {'next': {'next': null, 'value': 2}, 'value': 5}, 'value': 7}, 'value': 12}, 'value': 20}});
     });
   });
 });
